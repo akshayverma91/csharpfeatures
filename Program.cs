4199f36860f4c8e6c6e35b054060f332d199e354
Akshay Verma <akshayverma91@gmail.com>
2024-05-28 22:31:42 +0530

    Add files via upload
    initial commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Create a new canal lock:
+var canalGate = new CanalLock();
+
+// State should be doors closed, water level low:
+Console.WriteLine(canalGate);
+
+canalGate.SetLowGate(open: true);
+Console.WriteLine($"Open the lower gate:  {canalGate}");
+
+Console.WriteLine("Boat enters lock from lower gate");
+
+canalGate.SetLowGate(open: false);
+Console.WriteLine($"Close the lower gate:  {canalGate}");
+
+canalGate.SetWaterLevel(WaterLevel.High);
+Console.WriteLine($"Raise the water level: {canalGate}");
+
+canalGate.SetHighGate(open: true);
+Console.WriteLine($"Open the higher gate:  {canalGate}");
+
+Console.WriteLine("Boat exits lock at upper gate");
+Console.WriteLine("Boat enters lock from upper gate");
+
+canalGate.SetHighGate(open: false);
+Console.WriteLine($"Close the higher gate: {canalGate}");
+
+canalGate.SetWaterLevel(WaterLevel.Low);
+Console.WriteLine($"Lower the water level: {canalGate}");
+
+canalGate.SetLowGate(open: true);
+Console.WriteLine($"Open the lower gate:  {canalGate}");
+
+Console.WriteLine("Boat exits lock at upper gate");
+
+canalGate.SetLowGate(open: false);
+Console.WriteLine($"Close the lower gate:  {canalGate}");
+
+
+
+Console.WriteLine("=============================================");
+Console.WriteLine("     Test invalid commands");
+// Open "wrong" gate (2 tests)
+try
+{
+    canalGate = new CanalLock();
+    canalGate.SetHighGate(open: true);
+}
+catch (InvalidOperationException)
+{
+    Console.WriteLine("Invalid operation: Can't open the high gate. Water is low.");
+}
+Console.WriteLine($"Try to open upper gate: {canalGate}");
+
+
+
